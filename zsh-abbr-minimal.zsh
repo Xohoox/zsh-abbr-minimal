a466b329f6a0a595fff23b977e534f784a689a6d
Fynn Michell-Auli <40913208+Xohoox@users.noreply.github.com>
2024-01-30 14:28:43 +0100

    BUGFIX: append space when courser is not a the end of the line issue #1

--- zsh-abbr-minimal.zsh
+++ zsh-abbr-minimal.zsh
@@ -4,47 +4,48 @@ _abbrFile="$XDG_DATA_HOME/zsh/abbr"
 abbr_expand() {
 	local words=(${(z)LBUFFER})
 	local lastWord=$words[-1]
-	local match=$(grep "^${lastWord}=" "$_abbrFile" | cut -f 2- -d '=')
-	[ -n "$match" ] || { abbr_space; return }
-
 	local secondLastWord=${words[-2]}
 	local -i word_count=${#words}
+	local match=$(grep "^$lastWord=" "$_abbrFile" | cut -f 2- -d '=')
+
+	[ -n "$match" ] || return
 
 	if [ "$word_count" = 1 ]; then
-		LBUFFER="$match "
+		LBUFFER="$match"
 	elif [ -n "$secondLastWord" ]; then
 		# todo: $( - is in same word
-		[[ "$secondLastWord" =~ '[|&;]' ]] || { abbr_space; return }
+		[[ "$secondLastWord" =~ '[|&;]' ]] || return
 		words[-1]="$match"
-		LBUFFER="$words "
-	else
-		abbr_space
+		LBUFFER="$words"
 	fi
+
 }
 
-# expand and run command
+abbr_space() {
+	abbr_expand
+	LBUFFER="$LBUFFER "
+}
+
 abbr_enter() {
 	abbr_expand
 	zle accept-line
 }
 
 # appand space to LBUFFER
-abbr_space() {
+abbr_space_no_expand() {
 	LBUFFER="$LBUFFER "
 }
 
-# todo: fix keybinding
-# run command without expanding
-# abbr_enter_do_nothing() {
-# 	zle accept-line
-# }
+abbr_enter_no_expand() {
+	zle accept-line
+}
 
-zle -N abbr_expand
 zle -N abbr_space
+zle -N abbr_space_no_expand
 zle -N abbr_enter
-# zle -N abbr_enter_do_nothing
+zle -N abbr_enter_no_expand
 
-bindkey ' ' abbr_expand
-bindkey '^ ' abbr_space
+bindkey ' ' abbr_space
+bindkey '^ ' abbr_space_no_expand
 bindkey '^M' abbr_enter
-# bindkey '^[M' abbr_enter_do_nothing
+bindkey '^[M' abbr_enter_no_expand
